4c3165c81e3a10c95759910a66efa81b48a57e7c
Gabriel oliveira <gabeosz2020@gmail.com>
2026-04-16 13:41:14 -0300

    feat: adiciona Scanner para entrada de dados no console

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class ConsoleView {
     public void imprimeConsole(List<Transacao> lista, BigDecimal saldo) {
@@ -14,14 +15,28 @@ public class ConsoleView {
 
         System.out.println("===== LISTA DE TRANSAÇÕES =====");
 
-        for (Transacao t :lista ) {
+        for (Transacao t : lista) {
             System.out.println(t);
         }
 
+
         System.out.println("------------------------------");
         System.out.println("Saldo: " + formato.format(saldo));
+    }
 
+    private Scanner scanner = new Scanner(System.in);
 
+    public int lerOpcao() {
+        System.out.println("===== MENU =====");
+        System.out.println("1 - Adicionar transação ");
+        System.out.println("2 - Listar transações ");
+        System.out.println("3 - Ver saldo ");
+        System.out.print("0 - Sair ");
 
+        int opcao = scanner.nextInt();
+
+        return opcao;
     }
 }
+
+
